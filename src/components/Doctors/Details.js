0073e1efd766a4386f37eaced2f619928f35aaee
Nuri1977 <nurilacka@gmail.com>
2022-06-03 19:57:09 +0200

    Style detail page

--- src/components/Doctors/Details.js
+++ src/components/Doctors/Details.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { fetchOneDoctor } from '../../redux/bookDoctor/doctorThunks';
+import classes from './Detail.module.css';
 
 function Details() {
   const { id } = useParams();
@@ -13,14 +14,57 @@ function Details() {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1> Details</h1>
-      <h2>
-        { doctor.name }
-        {' '}
-      </h2>
-      <img alt="doctor" src={doctor.image_url} />
-    </div>
+    <section className={classes.doctor}>
+      <div className={classes.detail}>
+        <div className={classes.photo}>
+          <img alt="doctor" src={doctor.image_url} />
+        </div>
+        <div className={classes.info}>
+          <h2 className={classes.name}>
+            { doctor.name }
+          </h2>
+          <div className={classes.description}>
+            { doctor.description }
+          </div>
+          <div className={`${classes.item} ${classes.neutral}`}>
+            <div>City</div>
+            <div className={classes.city}>
+              { doctor.city }
+            </div>
+          </div>
+          <div className={`${classes.item}`}>
+            <div>Specialization</div>
+            <div className={classes.specialization}>
+              { doctor.specialization }
+            </div>
+          </div>
+          <div className={`${classes.item} ${classes.neutral}`}>
+            <div>Cost per day</div>
+            <div className={classes.cost_per_day}>
+              $
+              { doctor.cost_per_day }
+            </div>
+          </div>
+          <div className={classes.reserve}>
+            <button type="button" className={classes.btn}>
+              <i className="fa-solid fa-gear" />
+              Reserve
+              <i className="fa-regular fa-circle-right" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={classes.back}>
+        <Link to="/">
+          <button type="button" className={classes.btn1}>
+            <i className="fa-solid fa-caret-left" />
+          </button>
+        </Link>
+      </div>
+
+      <div />
+
+    </section>
   );
 }
 
