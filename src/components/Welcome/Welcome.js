76d6cb06ad352f8e447a9f521f6382609aab02fa
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-06 12:21:07 +0100

    added animation to the welcome page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import AnimateWrapper from '../AnimateWrapper';
 
 const Welcome = () => (
-  <div>Available Doctors</div>
+  <AnimateWrapper
+    initial={{ x: 300, opacity: 1 }}
+    animate={{ x: 0, opacity: 1 }}
+    exit={{ x: 300, opacity: 0 }}
+    transition={{ duration: 0.8 }}
+  >
+    <div>Available Doctors</div>
+  </AnimateWrapper>
 );
 
 export default Welcome;
