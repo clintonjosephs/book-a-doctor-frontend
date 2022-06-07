942bc2cef333e59807b8375295effc7a780d6e3a
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-07 08:22:08 +0100

    added social media icons to the doctors

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -4,6 +4,9 @@ import { fetchAllDoctors } from '../../redux/bookDoctor/doctorThunks';
 import AnimateWrapper from '../../components/AnimateWrapper';
 import 'react-multi-carousel/lib/styles.css';
 import styles from './Welcome.module.css';
+import facebook from '../../assets/images/facebook_icon.png';
+import twitter from '../../assets/images/twitter_icon.png';
+import instagram from '../../assets/images/instagram_icon.png';
 
 let fetched = false;
 
@@ -33,16 +36,25 @@ const Welcome = () => {
       exit={{ y: 1000, opacity: 0 }}
       transition={{ duration: 0.8 }}
     >
+      <div className={styles.top}>
+        <h1>Available Doctors</h1>
+        <span>Professionals ready to serve you</span>
+      </div>
       <section className={styles.doctorsCarousel}>
         <ul className={styles.carouselWrapper}>
           { displayDoctors.map(({
             id, name, imageUrl, description,
           }) => (
             <li key={id} className={styles.carouselItem}>
-              <img src={imageUrl} width={150} height={150} alt="doctor" />
+              <img src={imageUrl} alt="doctor" className={styles.slideImg} />
               <div className={styles.docName}>{name}</div>
               <div className={styles.line} />
-              <div>{description}</div>
+              <div className={styles.docDescription}>{description}</div>
+              <div className={styles.social}>
+                <img src={instagram} alt="instagram" />
+                <img src={facebook} alt="facebook" />
+                <img src={twitter} alt="twitter" />
+              </div>
             </li>
           )) }
         </ul>
