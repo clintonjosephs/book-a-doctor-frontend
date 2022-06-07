e2e023c8bd433ca6793154e90739bd97470f23da
Nuri1977 <nurilacka@gmail.com>
2022-06-07 12:12:47 +0200

    Update form data

--- src/components/Doctors/AddDoctor.js
+++ src/components/Doctors/AddDoctor.js
@@ -16,13 +16,20 @@ function addDoctorApi(data) {
 }
 
 function AddDoctor() {
+  function formData(event) {
+    const data = new FormData();
+    data.append('doctor[name]', event.target.name.value);
+    data.append('doctor[specialization]', event.target.specialization.value);
+    data.append('doctor[city]', event.target.city.value);
+    data.append('doctor[description]', event.target.description.value);
+    data.append('doctor[cost_per_day]', event.target.cost_per_day.value);
+    data.append('doctor[image]', event.target.image.files[0]);
+    return data;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    const data = new FormData();
-    console.log(data);
-
-    data.append('doctor[name]', event.target.name.value);
-    data.append('doctor[image]', event.target.image.files[0]);
+    const data = formData(event);
     addDoctorApi(data);
   }
 
@@ -32,6 +39,22 @@ function AddDoctor() {
       <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="name">name</label>
         <input type="text" name="name" id="name" />
+        <br />
+
+        <label htmlFor="specialization">specialization</label>
+        <input type="text" name="specialization" id="specialization" />
+        <br />
+
+        <label htmlFor="city">city</label>
+        <input type="text" name="city" id="city" />
+        <br />
+
+        <label htmlFor="description">description</label>
+        <input type="text" name="description" id="description" />
+        <br />
+
+        <label htmlFor="cost_per_day">Cost per day</label>
+        <input type="number" name="cost_per_day" id="cost_per_day" />
         <br />
 
         <label htmlFor="image">Image</label>
