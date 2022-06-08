d4dacfdf54f75d03d762e0b77418790afd86e329
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-08 15:03:38 +0100

    resolved conflict in the doctorActions.js file

--- src/redux/bookDoctor/doctorActions.js
+++ src/redux/bookDoctor/doctorActions.js
@@ -5,14 +5,11 @@ const ADD_DOCTOR = 'doctors/ADD_DOCTOR';
 const API_ERROR = 'doctors/API_ERROR';
 const USER_DETAILS = 'doctors/USER_DETAILS';
 const API_LOADING = 'doctors/API_LOADING';
-<<<<<<< HEAD
 const CHUNKED_DOCTORS = 'doctors/CHUNKED_DOCTORS';
 const CAROUSEL_STATE = 'doctors/CAROUSEL_STATE';
-=======
 const SET_CURRENT_DOCTOR = 'doctors/SET_CURRENT_DOCTOR';
 export const ADD_APPOINTMENT = 'doctors/ADD_APPOINTMENT';
 export const ADD_APPOINTMENT_FAILURE = 'doctors/ADD_APPOINTMENT_FAILURE';
->>>>>>> develop
 
 // initial state
 const bookDoctorState = {
@@ -55,11 +52,11 @@ export const loadChunkedDoctors = (payload) => ({
   payload,
 });
 
-<<<<<<< HEAD
 export const updateCarouselState = (payload) => ({
   type: CAROUSEL_STATE,
   payload,
-=======
+});
+
 export const setCurrentDoctor = (data) => ({
   type: SET_CURRENT_DOCTOR,
   payload: data,
@@ -73,7 +70,6 @@ export const addAppointment = (data) => ({
 export const addAppointmentFailure = (data) => ({
   type: ADD_APPOINTMENT,
   payload: data,
->>>>>>> develop
 });
 
 // ... other synchronous actions goes here
@@ -95,19 +91,16 @@ const bookDoctorReducer = (state = bookDoctorState, { type, payload }) => {
       return { ...state, loading: !state.loading };
     case USER_DETAILS:
       return { ...state, userDetails: payload };
-<<<<<<< HEAD
     case CHUNKED_DOCTORS:
       return { ...state, doctorsChunked: [...payload], currentCarouselState: [...payload[0]] };
     case CAROUSEL_STATE:
       return { ...state, currentCarouselState: [...state.doctorsChunked[payload]] };
-=======
     case SET_CURRENT_DOCTOR:
       return { ...state, doctor: [payload] };
     case ADD_APPOINTMENT:
       return { ...state, doctor: null, appointments: [...state.appointments, payload] };
     case ADD_APPOINTMENT_FAILURE:
       return { ...state, error: true, message: payload };
->>>>>>> develop
     default:
       return state;
   }
