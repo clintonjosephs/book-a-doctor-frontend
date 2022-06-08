6024e26e9653b87781e7574021e965bddcea2266
jihaneh <jijih@hotmail.fr>
2022-06-08 15:51:48 +0200

    Fix bug on appointment

--- src/redux/bookDoctor/doctorActions.js
+++ src/redux/bookDoctor/doctorActions.js
@@ -84,7 +84,7 @@ const bookDoctorReducer = (state = bookDoctorState, { type, payload }) => {
     case USER_DETAILS:
       return { ...state, userDetails: payload };
     case SET_CURRENT_DOCTOR:
-      return { ...state, doctor: payload };
+      return { ...state, doctor: [payload] };
     case ADD_APPOINTMENT:
       return { ...state, doctor: null, appointments: [...state.appointments, payload] };
     case ADD_APPOINTMENT_FAILURE:
