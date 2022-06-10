df94596efb3701d9d786da22fe3894a0fe540043
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-10 12:05:48 +0100

    setup encryption and decryption functions

--- src/helpers/format/format.js
+++ src/helpers/format/format.js
@@ -26,3 +26,27 @@ export const userStatus = () => {
   }
   return true;
 };
+
+
+export const encryptData = (o, salt) => {
+  o = JSON.stringify(o).split('');
+  for(var i = 0, l = o.length; i < l; i++)
+      if(o[i] == '{')
+          o[i] = '}';
+      else if(o[i] == '}')
+          o[i] = '{';
+  return encodeURI(salt + o.join(''));
+}
+
+export const decryptData = (o, salt) => {
+  o = decodeURI(o);
+  if(salt && o.indexOf(salt) != 0)
+      throw new Error('object cannot be decrypted');
+  o = o.substring(salt.length).split('');
+  for(var i = 0, l = o.length; i < l; i++)
+      if(o[i] == '{')
+          o[i] = '}';
+      else if(o[i] == '}')
+          o[i] = '{';
+  return JSON.parse(o.join(''));
+}
