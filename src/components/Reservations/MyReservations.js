f6ffd99c1452f39bbd4151e08fa1e25b2080330b
atillatahak <atillatahaa@gmail.com>
2022-06-10 12:25:00 +0300

    changed style for table

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -9,15 +9,16 @@ function MyReservations() {
   const dispatch = useDispatch();
   const reservation = useSelector((state) => state.reservationReducer);
   useEffect(() => {
-    dispatch(getReservations());
+    if (reservation.length === 0) {
+      dispatch(getReservations());
+    }
   }, []);
 
   return (
-    <div>
+    <section className={style.reservation}>
       <table className={style.table} id="table">
         <thead>
           <tr>
-            <th>#</th>
             <th>Doctor Picture</th>
             <th>Doctor Name</th>
             <th>Doctor Special</th>
@@ -28,9 +29,8 @@ function MyReservations() {
         <tbody>
           {reservation.length === 0
             ? <tr><td>There is no Reservations</td></tr>
-            : reservation.map((element, i) => (
+            : reservation.map((element) => (
               <tr key={element.id}>
-                <td>{i + 1}</td>
                 <td><img src={element.imageUrl} alt="doctors" className={style.img} /></td>
                 <td>{element.doctor.name}</td>
                 <td>{element.doctor.specialization}</td>
@@ -40,8 +40,7 @@ function MyReservations() {
             ))}
         </tbody>
       </table>
-    </div>
-
+    </section>
   );
 }
 
