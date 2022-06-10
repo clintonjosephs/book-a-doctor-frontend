57e0f6373ae13c2c99beca9dbd02f8f28b237b17
atillatahak <atillatahaa@gmail.com>
2022-06-10 10:35:59 +0300

    remove border added

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -26,16 +26,18 @@ function MyReservations() {
           </tr>
         </thead>
         <tbody>
-          {reservation && reservation.map((element, i) => (
-            <tr key={element.id}>
-              <td>{i + 1}</td>
-              <td><img src={element.imageUrl} alt="doctors" className={style.img} /></td>
-              <td>{element.doctor.name}</td>
-              <td>{element.doctor.specialization}</td>
-              <td>{convertDateWithName(element.date_of_appointment)}</td>
-              <td><button type="button" className={style.button}>Cancel</button></td>
-            </tr>
-          ))}
+          {reservation.length === 0
+            ? <tr><td>There is no Reservations</td></tr>
+            : reservation.map((element, i) => (
+              <tr key={element.id}>
+                <td>{i + 1}</td>
+                <td><img src={element.imageUrl} alt="doctors" className={style.img} /></td>
+                <td>{element.doctor.name}</td>
+                <td>{element.doctor.specialization}</td>
+                <td>{convertDateWithName(element.date_of_appointment)}</td>
+                <td><button type="button" className={style.button}>Cancel</button></td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
